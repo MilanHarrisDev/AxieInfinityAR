f8f5eae04552ba3358fbed75d4af39315857f7d2
Milan Harris <milanharrisdev@gmail.com>
2019-02-07 17:50:57 -0600

    getAxieImage cloud function update
    - badded parameter for image type
    - function can now be used to get the static idle image, or the axie sprite sheet images (axie or spirit sprite sheets)

--- CloudFunctions/functions/index.js
+++ CloudFunctions/functions/index.js
@@ -4,7 +4,8 @@ const functions = require('firebase-functions');
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
-//
+
+
 exports.getAxieImage = functions.https.onRequest((req, res) => {
     var url = 'https://api.axieinfinity.com/v1/axies/';
     
@@ -12,9 +13,22 @@ exports.getAxieImage = functions.https.onRequest((req, res) => {
     //https://samples.openweathermap.org/data/2.5/weather?q=Dallas&appid=b6907d289e10d714a6e88b30761fae22
 
     request(url + req.query.axieId, function (error, response, body) {
-        if (!error && response.statusCode == 200) {
+        if (!error && response.statusCode == 200) 
+        {
             var jsonBody = JSON.parse(body);
-            res.status(200).send('<img src="' + jsonBody.figure.static.idle + '">');
+            
+            switch(req.query.type)
+            {
+                case 'axie':
+                    res.status(200).send(jsonBody.figure.axie.image);
+                    break;
+                case 'static':
+                    res.status(200).send(jsonBody.figure.static.idle);
+                    break;
+                case 'spirit':
+                    res.status(200).send(jsonBody.figure.spirit.image);
+                    break;
+            }
         }
     });
 });
