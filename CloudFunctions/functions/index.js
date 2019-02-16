52f2bdc05440db02aef49b06c373b1e8f680bb0a
Milan Harris <milanharrisdev@gmail.com>
2019-02-16 05:42:30 -0600

    Added Cloud Function - getAxieImageAtlas

--- CloudFunctions/functions/index.js
+++ CloudFunctions/functions/index.js
@@ -1,17 +1,13 @@
 var request = require('request');
 
 const functions = require('firebase-functions');
+const url = 'https://api.axieinfinity.com/v1/axies/';
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 
-
-exports.getAxieImage = functions.https.onRequest((req, res) => {
-    var url = 'https://api.axieinfinity.com/v1/axies/';
-    
-    //response.status(200).send(req.query.axieId);
-    //https://samples.openweathermap.org/data/2.5/weather?q=Dallas&appid=b6907d289e10d714a6e88b30761fae22
-
+//returns link to image file
+exports.getAxieImage = functions.https.onRequest((req, res) => {  
     request(url + req.query.axieId, function (error, response, body) {
         if (!error && response.statusCode == 200) 
         {
@@ -27,6 +23,27 @@ exports.getAxieImage = functions.https.onRequest((req, res) => {
                     break;
                 case 'spirit':
                     res.status(200).send(jsonBody.figure.spirit.image);
+                    break;
+                    
+            }
+        }
+    });
+});
+
+//returns link to atlas file
+exports.getAxieImageAtlas = functions.https.onRequest((req, res) =>{
+    request(url + req.query.axieId, function (error, response, body) {
+        if (!error && response.statusCode == 200) 
+        {
+            var jsonBody = JSON.parse(body);
+            
+            switch(req.query.type)
+            {
+                case 'axie':
+                    res.status(200).send(jsonBody.figure.axie.atlas);
+                    break;
+                case 'spirit':
+                    res.status(200).send(jsonBody.figure.spirit.atlas);
                     break;
             }
         }
